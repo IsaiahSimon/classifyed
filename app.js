c4e43dbee7bf6e5199a386c8d3e4093c73208504
Isaiah Simon <60068759+IsaiahSimon@users.noreply.github.com>
2022-06-07 14:31:18 -0400

    Level 2 - Encryption and Environment Vars

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb://localhost:27017/userDB");
+mongoose.connect('mongodb://localhost:27017/userDB');
 
-const userSchema = {
+const userSchema = new mongoose.Schema ({
   email: String,
   password: String
-};
+});
 
-const User = mongoose.model("User", userSchema);
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']});
+
+const User = mongoose.model('User', userSchema);
 
 
 app.get('/', (req, res) => {
@@ -31,7 +35,7 @@ app.get('/register', (req, res) => {
   res.render('register');
 });
 
-app.post("/register", (req, res) => {
+app.post('/register', (req, res) => {
   const newUser = new User({
     email: req.body.username,
     password: req.body.password
@@ -41,12 +45,12 @@ app.post("/register", (req, res) => {
     if (err) {
       console.log(err);
     } else {
-      res.render("secrets"); // only render secrets page if user is registered
+      res.render('secrets'); // only render secrets page if user is registered
     }
   });
 });
 
-app.post("/login", (req, res) => {
+app.post('/login', (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
@@ -56,7 +60,7 @@ app.post("/login", (req, res) => {
     } else {
       if(foundUser) {
         if (foundUser.password === password) {
-          res.render("secrets");
+          res.render('secrets');
         }
       }
     }
