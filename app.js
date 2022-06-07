b3850a145e9d33b876f93d71be9b5618312ee393
Isaiah Simon <60068759+IsaiahSimon@users.noreply.github.com>
2022-06-07 17:10:19 -0400

    Level 4 - Hashing and Salting with bcrypt
    - Removed md5
    - Add bcrypt
    - Rename website

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -18,7 +19,6 @@ const userSchema = new mongoose.Schema ({
   password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = mongoose.model('User', userSchema);
 
@@ -36,17 +36,19 @@ app.get('/register', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  })
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    })
 
-  newUser.save((err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render('secrets'); // only render secrets page if user is registered
-    }
+    newUser.save((err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render('secrets'); // only render secrets page if user is registered
+      }
+    });
   });
 });
 
@@ -59,9 +61,11 @@ app.post('/login', (req, res) => {
       console.log(err);
     } else {
       if(foundUser) {
-        if (foundUser.password === password) {
-          res.render('secrets');
-        }
+        bcrypt.compare(password, foundUser.password, function (err, result) {
+          if (result == true) {
+            res.render('secrets'); // only render secrets page if user is logged in
+          }
+        });
       }
     }
   });
